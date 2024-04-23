186d247b99443d0fff976af02db63059be2bd30b
elenaivadreyer <E.Dreyer@students.hertie-school.org>
2024-04-23 14:06:06 +0200

    major changes flask app
    user login, logout, all navigation

---
+++ flask-debt-app/app.py
@@ -0,0 +1,11 @@
+# app.py
+from app import create_app
+from flask_login import LoginManager
+
+app = create_app()
+
+if __name__ == '__main__':
+    with app.app_context():
+        from app import db
+        db.create_all()  # Create database tables
+    app.run(debug=True)
