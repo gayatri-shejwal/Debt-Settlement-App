0bd2b39bd79dda8973c2cb70f0f157ead7a06f00
Minho Kang <rkdalsgh32@gmail.com>
2024-05-05 18:48:34 +0200

    Clear folders

--- flask-elena/costsplitter/forms.py
+++
@@ -1,9 +0,0 @@
-# flaskapp/forms.py
-from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField
-from wtforms.validators import DataRequired
-
-class ExpenseForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
-    amount = DecimalField('Amount', validators=[DataRequired()])
--- flask-elena/instance/config.py
+++
@@ -1,6 +0,0 @@
-# flaskapp/config.py
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '020720000'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
